6ab73d686882e22152742f9014208e96173dc730
hugo dupuis <hugo.dupuis2@ecoles-epsi.net>
2025-06-10 21:08:59 +0200

    First commit

---
+++ src/test/java/com/testalten/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testalten.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
